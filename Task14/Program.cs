8d81a1ffd63a99227ce7a07c9d63087fe566976a
Anton S <geodesist.sushkov@gmail.com>
2021-12-14 14:03:51 +0300

    Добавьте файлы проекта.

---
+++ Task14/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Task14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+            cat.ShowInfo();
+            Dog dog = new Dog();
+            dog.ShowInfo();
+
+            Console.ReadKey();
+        }
+    }
+
+    abstract class Animal
+    {
+        public abstract string Name { get; }
+        protected abstract string GetName();
+        public Animal(string name)
+        {
+        }
+        public abstract void Say();
+        public void ShowInfo()
+        {
+            Console.Write(GetName() + ":");
+            Say();
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override string Name => "кошка";
+
+        public Cat() : base("")
+        {
+        }
+
+        protected override string GetName()
+        {
+            return Name;
+        }
+
+        public override void Say()
+        {
+            Console.WriteLine("мяу");
+        }
+    }
+    class Dog : Animal
+    {
+        public override string Name => "собака";
+
+        public Dog() : base("")
+        {
+        }
+        protected override string GetName()
+        {
+            return Name;
+        }
+        public override void Say()
+        {
+            Console.WriteLine("гав");
+        }
+    }
+}
+
